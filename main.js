60a478e90c96427d30ae7b3d4f77a683177d0ee3
Victor Izquierdo <vhmizq@gmail.com>
2023-04-10 18:02:25 -0600

    validaiton modified

--- main.js
+++ main.js
@@ -1,32 +1,51 @@
-let item = (ele) => document.querySelectorAll(ele);
+let item = (ele) => document.getElementsByClassName(ele);
 let getField = (ele) => document.getElementById(ele);
 
-const $errorMessage = item('error');
+const $errorName = item('error-name');
+const $errorLastName = item('error-last');
+const $errorEmail = item('error-email');
+const $errorPassword = item('error-password');
+
 const $firstName = getField('firstName');
 const $lastName = getField('lastName');
 const $email = getField('email');
 const $password = getField('password');
+const $submitBtn = getField('submit');
+const $form = getField('form');
 
 const validateStrings = (field) => {
-  const isEmptyValue = field.value === '';
-  isEmptyValue ? true : false;
-}
+  const isEmptyValue = field.value.trim() === '';
+  return !isEmptyValue;
+};
 
 const validateEmail = (email) => {
   const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-  const isMatchValue = email.match(regex);
-  isMatchValue ? true : false;
-}
+  const isMatchValue = email.value.match(regex);
+  return isMatchValue !== null;
+};
+
+$form.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
 
 const handleValidation = () => {
-  const isMissingValidation = validateEmail($email);
-  if (isMissingValidation) {
-    re
+  const isMissingEmail = !validateEmail($email);
+  const isMissingFirstName = !validateStrings($firstName);
+  const isMissingLastName = !validateStrings($lastName);
+  const isMissingPassword = !validateStrings($password);
+
+  if (isMissingEmail) {
+    $errorEmail.style.display = 'block';
   }
-}
+  if (isMissingFirstName) {
+    $errorName.style.display = 'block';
+  }
+  if (isMissingLastName) {
+    $errorLastName.style.display = 'block';
+  }
+  if (isMissingPassword) {
+    $errorPassword.style.display = 'block';
+  }
+};
 
-handleValidation();
-
-
-
-
+$submitBtn.addEventListener('submit', handleValidation());
