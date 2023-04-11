c24d5fb9dc4b52342ebfff6a6c21239028af0f02
Victor Izquierdo <vhmizq@gmail.com>
2023-04-10 18:39:29 -0600

    update js

--- main.js
+++ main.js
@@ -1,51 +1,47 @@
-let item = (ele) => document.getElementsByClassName(ele);
 let getField = (ele) => document.getElementById(ele);
 
-const $errorName = item('error-name');
-const $errorLastName = item('error-last');
-const $errorEmail = item('error-email');
-const $errorPassword = item('error-password');
+const $firstName = getField('firstName').value;
+const $lastName = getField('lastName').value;
+const $email = getField('email').value;
+const $password = getField('password').value;
 
-const $firstName = getField('firstName');
-const $lastName = getField('lastName');
-const $email = getField('email');
-const $password = getField('password');
+const $errorName = getField('error-name');
+const $errorLast = getField('error-last');
+const $errorEmail = getField('error-email');
+const $errorPassword = getField('error-password');
+
 const $submitBtn = getField('submit');
-const $form = getField('form');
 
-const validateStrings = (field) => {
-  const isEmptyValue = field.value.trim() === '';
-  return !isEmptyValue;
-};
-
-const validateEmail = (email) => {
-  const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
-  const isMatchValue = email.value.match(regex);
-  return isMatchValue !== null;
-};
-
-$form.addEventListener('submit', (e) => {
+$submitBtn.addEventListener('submit', (e) => {
   e.preventDefault();
+  validateName();
+  validateLastName();
+  validatePassword();
+  validateEmail();
 });
 
-const handleValidation = () => {
-  const isMissingEmail = !validateEmail($email);
-  const isMissingFirstName = !validateStrings($firstName);
-  const isMissingLastName = !validateStrings($lastName);
-  const isMissingPassword = !validateStrings($password);
-
-  if (isMissingEmail) {
-    $errorEmail.style.display = 'block';
-  }
-  if (isMissingFirstName) {
+const validateName = () => {
+  if ($firstName.trim() === '') {
     $errorName.style.display = 'block';
   }
-  if (isMissingLastName) {
-    $errorLastName.style.display = 'block';
+};
+
+const validateLastName = () => {
+  if ($lastName.trim() === '') {
+    $errorLast.style.display = 'block';
   }
-  if (isMissingPassword) {
+};
+
+const validatePassword = () => {
+  if ($password.trim() === '') {
     $errorPassword.style.display = 'block';
   }
 };
 
-$submitBtn.addEventListener('submit', handleValidation());
+const validateEmail = () => {
+  const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  const isMatchEmail = $email.match(regex);
+  if (!isMatchEmail) {
+    $errorEmail.style.display = 'block';
+  }
+};
